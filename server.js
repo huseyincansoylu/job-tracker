44ea43b30d1318dbafc7c3109e534e133d891313
huseyin <huseyin@keyko.io>
2022-07-12 15:51:50 +0300

    loign user server

--- server.js
+++ server.js
@@ -1,46 +1,48 @@
-import "express-async-errors"
+import "express-async-errors";
 
-import express from "express"
-const app = express()
+import express from "express";
+const app = express();
 
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
+
+import morgan from "morgan";
 
 //db and authenticateUser
-import connectDB from "./db/connect.js"
-
+import connectDB from "./db/connect.js";
 
 //routers
-import authRouter from "./routes/authRoutes.js"
-import jobRouter from "./routes/jobsRoutes.js"
+import authRouter from "./routes/authRoutes.js";
+import jobRouter from "./routes/jobsRoutes.js";
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 //middleware
-import errorHandlerMiddleware from "./middleware/error-handler.js"
-import notFoundMiddleware from "./middleware/not-found.js"
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import notFoundMiddleware from "./middleware/not-found.js";
 
-app.use(express.json())
-
-
-app.use("/api/v1/auth", authRouter)
-app.use("/api/v1/jobs", jobRouter)
-
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
-
-
-const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URL)
-
-        app.listen(port, () => {
-            console.log(`Server is listening on port:${port}`);
-        })
-    } catch (error) {
-        console.log(error)
-    }
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
 }
 
+app.use(express.json());
 
-start()
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+
+    app.listen(port, () => {
+      console.log(`Server is listening on port:${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
