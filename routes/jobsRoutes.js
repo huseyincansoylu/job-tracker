50a5e7c09bb9e256338efc537d9b8f13bbf04f0e
huseyin <huseyin@keyko.io>
2022-07-14 00:13:36 +0300

    auth setup

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,10 +1,16 @@
-import express from "express"
-const router = express.Router()
+import express from "express";
+const router = express.Router();
 
-import { createJob, getAllJobs, deleteJob, showStats, updateJob } from "../controllers/jobsController.js"
+import {
+  createJob,
+  getAllJobs,
+  deleteJob,
+  showStats,
+  updateJob,
+} from "../controllers/jobsController.js";
 
-router.route("/").post(createJob).get(getAllJobs)
-router.route("/stats").get(showStats)
-router.route("/:id").delete(deleteJob).patch(updateJob)
+router.route("/").post(createJob).get(getAllJobs);
+router.route("/stats").get(showStats);
+router.route("/:id").delete(deleteJob).patch(updateJob);
 
-export default router
+export default router;
--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const port = process.env.PORT || 5000;
 //middleware
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
+import authenticateUser from "./middleware/auth.js";
 
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
@@ -28,7 +29,7 @@ if (process.env.NODE_ENV !== "production") {
 app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", jobRouter);
+app.use("/api/v1/jobs", authenticateUser, jobRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
