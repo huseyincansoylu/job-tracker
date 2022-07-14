61e8a528e09cdb99c87ff531a7cfb7700af9a7a4
huseyin <huseyin@keyko.io>
2022-07-14 21:00:59 +0300

    added setEdit functionality

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -21,6 +21,7 @@ import {
   CREATE_JOB_ERROR,
   GET_JOBS_BEGIN,
   GET_JOBS_SUCCESS,
+  SET_EDIT_JOB,
 } from "./actions";
 
 const initialState = {
@@ -236,7 +237,11 @@ const AppProvider = ({ children }) => {
   };
 
   const setEditJob = (id) => {
-    console.log(`set edit job : ${id}`);
+    dispatch({ type: SET_EDIT_JOB, payload: { id } });
+  };
+
+  const editJob = () => {
+    console.log("edit job");
   };
 
   const deleteJob = (id) => {
@@ -258,6 +263,7 @@ const AppProvider = ({ children }) => {
         toggleSidebar,
         handleChange,
         clearValues,
+        editJob,
       }}
     >
       {children}
