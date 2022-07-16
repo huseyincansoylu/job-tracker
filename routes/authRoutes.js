c0e4d4bb96ae0aa89689aa6a04531959636fb0ac
huseyin <huseyin@keyko.io>
2022-07-16 19:07:55 +0300

    commit heroku

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-import rateLimit from "express-rate-limit";
+import rateLimiter from "express-rate-limit";
 
 const apiLimiter = rateLimiter({
   windowMs: 15 * 60 * 1000, // 15 minutes
